bc063352de3837148c22d61d552fbdc175afd37c
Abhinav Srivastava <as@scoar.in>
2020-08-27 18:19:12 +0530

    Updates

---
+++ precache-manifest.567e67a08bddc2cf60830388e9aad6eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8db22832090d1be927d17b6a17fa60",
+    "url": "/MarketApp/index.html"
+  },
+  {
+    "revision": "21b97d3485aa94eadc48",
+    "url": "/MarketApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "21d72e4567d5076fa7ee",
+    "url": "/MarketApp/static/js/2.b53112c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MarketApp/static/js/2.b53112c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b97d3485aa94eadc48",
+    "url": "/MarketApp/static/js/main.0db4487e.chunk.js"
+  },
+  {
+    "revision": "3aa1582eb3f3aabe3387",
+    "url": "/MarketApp/static/js/runtime-main.19ff0151.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MarketApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
